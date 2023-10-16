d37c36fce1123e12851059e56f4c9442a3331d58
Tracey <ttrepper@yahoo.com>
2023-10-15 20:11:07 -0400

    add weather for single day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather defaultCity="New York" />
+      <div className="container">
+        <footer>
+          <a
+            href="https://github.com/tbtreppe/weather-react-app"
+            target="_blank"
+          >
+            Open Source Code
+          </a>
+          , by Tracey Reis
+        </footer>
+      </div>
     </div>
   );
 }
